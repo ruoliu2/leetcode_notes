3ac4799974e53d7a2e64c900b66235e71dbe1d12
Ruo Liu <ruoliu2@illinois.edu>
2023-10-20 19:15:55 -0500

    update graph

--- graph/bfs.py
+++ graph/bfs.py
@@ -1,6 +1,25 @@
 # another problem, two places to check visit
 from typing import List
 import collections
+from itertools import product
+from collections import deque
+
+
+class Solution:
+    def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        queue, safeness = deque(), [[-1] * n for _ in range(n)]
+        unseen = set(product(range(n), range(n)))
+        def nei(x, y): return set(
+            ((x-1, y), (x, y-1), (x+1, y), (x, y+1))) & unseen
+
+        queue.append((0, 0, 0))
+        unseen.discard((0, 0))
+        while queue:
+            s, x, y = queue.popleft()
+            for nx, ny in nei(x, y):
+                unseen.discard((nx, ny))
+                queue.append((s+1, nx, ny))
 
 
 class Solution1:  # prefered
--- graph/dijkstra.py
+++ graph/dijkstra.py
@@ -2,9 +2,34 @@
 import collections
 import heapq
 from typing import List
+from itertools import product
+from heapq import heappop, heappush
+from collections import deque
 
 
-class Solution:
+class Solution1:
+    def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
+        n, m = len(grid), len(grid[0])
+        queue, safeness = deque(), [[-1] * n for _ in range(n)]
+        unseen = set(product(range(n), range(n)))
+
+        def nei(x, y): return set(
+            ((x-1, y), (x, y-1), (x+1, y), (x, y+1))) & unseen
+
+        hp = [(-safeness[-1][-1], n-1, m-1)]
+        unseen.discard((n-1, m-1))
+        while hp:
+            s, x, y = heappop(hp)
+            if (x, y) == (0, 0):
+                print(min(-s, safeness[0][0]))
+            for nx, ny in nei(x, y):
+                safe = max(s, -safeness[nx][ny])
+                heappush(hp, (safe, nx, ny))
+                unseen.discard((nx, ny))
+        return -1
+
+
+class Solution2:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         graph = collections.defaultdict(list)
         for u, v, w in times:
