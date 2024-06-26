42a2441c386e09e5e442243cb89a42bd12dbf12b
Ruo Liu <ruoliu.dev@gmail.com>
2024-06-25 20:08:10 -0500

    1d solution for LIS

--- python/dp/LIS.py
+++ python/dp/LIS.py
@@ -1,19 +1,31 @@
 class Solution:
-    def lengthOfLIS(self, nums: list[int]) -> int:
+    def lengthOfLIS1D(self, nums: list[int]) -> int:
+        # f[i] = 1 + max(f[j] for j in range(i + 1, n) if nums[j] > nums[i])
+        dp = [1] * len(nums)
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[j] > nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+
+    def lengthOfLIS2D(self, nums: list[int]) -> int:
+        # cnt[i, j] LIS s.t LIS in nums[0...i] (not includes i) less than nums[j]
+        # cnt[i, j] = cnt[i - 1, j]
+        # max(cnt[i, j], cnt[i - 1, i] + 1) if nums[i] < nums[j]
+
+        # base case cnt[0, k] = 0
         n = len(nums)
-        nums.append(float("inf"))
-
-        dp = [[0] * (n + 1) for i in range(n)]
-        # dp[i][j] = length of LIS in nums[:i] with last element < nums[j]
-        for i in range(n):
-            for j in range(i + 1, n + 1):
-                dp[i][j] = dp[i - 1][j]
+        cnt = [[0] * n for i in range(n - 1)]
+        res = 0
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                cnt[i][j] = cnt[i - 1][j]
                 if nums[i] < nums[j]:
-                    dp[i][j] = max(dp[i][j], dp[i - 1][i] + 1)
+                    cnt[i][j] = max(cnt[i][j], cnt[i - 1][i] + 1)
+                res = max(res, cnt[i][j])
+        return res + 1
 
-        return dp[-1][-1]
-
-    def LIS(self, nums):
+    def lengthOfLISStack(self, nums):
         sub = []
         for num in nums:
             i = bisect_left(sub, num)
