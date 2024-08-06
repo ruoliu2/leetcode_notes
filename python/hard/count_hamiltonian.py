2eccb610eb318cba15da5ac319ee337341e19191
Ruo Liu <ruoliu.dev@gmail.com>
2024-08-05 21:13:41 -0500

    cache

--- python/hard/count_hamiltonian.py
+++ python/hard/count_hamiltonian.py
@@ -7,28 +7,23 @@ class Solution:
         n, MOD = len(nums), 10**9 + 7
         nei = defaultdict(list)
         nums.sort()
-        ret = 0
         for i in range(n):
-            for j in range(i+1, n):
+            for j in range(i + 1, n):
                 if nums[j] % nums[i] == 0:
                     nei[j].append(i)
                     nei[i].append(j)
         nei[-1] = range(n)
 
-        dp = [[-1 for _ in range(1 << n)] for _ in range(n)]
-
+        @cache
         def dfs(prev, mask):
             if mask == (1 << n) - 1:
-                dp[prev][mask] = 1
                 return 1
-            if dp[prev][mask] != -1:
-                return dp[prev][mask]
             count = 0
             for i in nei[prev]:
                 if not (mask & (1 << i)):  # i is not used in dfs
                     count += dfs(i, mask | (1 << i))
-            dp[prev][mask] = count
             return count
+
         return dfs(-1, 0) % MOD
 
 
