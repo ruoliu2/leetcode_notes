812d4bbbf661912acc682a799cc6ac1ab9e752ac
Ruo Liu <ruoliu.dev@gmail.com>
2024-08-10 22:39:06 -0500

    sieve

--- python/util/sieve.py
+++ python/util/sieve.py
@@ -1,11 +1,9 @@
 def sieve(n):
-    prime = [1] * (n + 1)
-    p = 2
-    while p * p <= n:  # if prime[p] unchanged, then its prime
-        if prime[p] == 1:  # update all multiples of p
-            for i in range(p * p, n + 1, p):
-                prime[i] = 0
-        p += 1
-
-    # return all primes
-    return [p for p in range(2, n) if prime[p] == 1]
+    primes = []
+    is_prime = [1] * (n + 1)
+    for i in range(2, n + 1):
+        if is_prime[i]:
+            primes.append(i)
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = 0
+    return primes
