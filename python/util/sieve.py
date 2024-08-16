4d59f2d61002aac4bd543cc1a33e4758f43e893d
Ruo Liu <ruoliu.dev@gmail.com>
2024-08-16 16:43:09 -0500

    prime

--- python/util/sieve.py
+++ python/util/sieve.py
@@ -2,8 +2,9 @@ def sieve(n):
     primes = []
     is_prime = [1] * (n + 1)
     for i in range(2, n + 1):
-        if is_prime[i]:
-            primes.append(i)
-            for j in range(i * i, n + 1, i):
-                is_prime[j] = 0
+        if not is_prime[i]:
+            continue
+        primes.append(i)
+        for j in range(i * i, n + 1, i):
+            is_prime[j] = 0
     return primes
