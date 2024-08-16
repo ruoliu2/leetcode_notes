5ca1abea66b1e52a5ca63a738f127f2182b9644f
Ruo Liu <ruoliu.dev@gmail.com>
2024-08-15 23:26:12 -0500

    rename snake case

--- python/sorting/quick_sort.py
+++ python/sorting/quick_sort.py
@@ -2,7 +2,7 @@ nums = []
 k = 0
 
 
-def quickSort(l, r):
+def quick_sort(l, r):
     if r < l:
         return
     pivot, p = nums[r], l
@@ -13,11 +13,11 @@ def quickSort(l, r):
             p += 1
     # Move pivot in-between left & right sides
     nums[p], nums[r] = nums[r], nums[p]
-    quickSort(l, p - 1)  # Quick sort left side
-    quickSort(p + 1, r)  # Quick sort right side
+    quick_sort(l, p - 1)  # Quick sort left side
+    quick_sort(p + 1, r)  # Quick sort right side
 
 
-def quickSelect(l, r):
+def quick_select(l, r):
     if r < l:
         return
     pivot, p = nums[r], l
@@ -27,6 +27,6 @@ def quickSelect(l, r):
             p += 1
     nums[p], nums[r] = nums[r], nums[p]
     if p > k:
-        quickSelect(l, p - 1)
+        quick_select(l, p - 1)
     elif p < k:
-        quickSelect(p + 1, r)
+        quick_select(p + 1, r)
