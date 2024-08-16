5f00073bd868e173b0b1625fc1ed4c613920bd27
Ruo Liu <ruoliu.dev@gmail.com>
2024-08-15 23:33:49 -0500

    fix bug

--- python/sorting/merge_sort.py
+++ python/sorting/merge_sort.py
@@ -1,4 +1,7 @@
-def merge(arr, s, m, e):
+arr = []
+
+
+def merge(s, m, e):
     # Copy the sorted left & right halfs to temp arrays
     l, r = arr[s : m + 1], arr[m + 1 : e + 1]
     i = j = 0  # index for l & r
@@ -13,16 +16,18 @@ def merge(arr, s, m, e):
         k += 1
     # one of the two will have remaining
     if i < len(l):
-        arr[k:] = l[i:]
+        arr[k : e + 1] = l[i : e + 1]
     else:
-        arr[k:] = r[j:]
+        arr[k : e + 1] = r[j : e + 1]
 
 
-def merge_sort(arr, s, e):
+def merge_sort(s, e):
     if e <= s:
         return arr
     m = (s + e) // 2
-    merge_sort(arr, s, m)
-    merge_sort(arr, m + 1, e)
-    merge(arr, s, m, e)
-    return arr
+    merge_sort(s, m)
+    merge_sort(m + 1, e)
+    merge(s, m, e)
+
+
+merge_sort(0, len(arr) - 1)
