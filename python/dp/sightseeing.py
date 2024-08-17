2321a7a443a6c4aa2f548bbebdaf8e9dfe0ddde4
Ruo Liu <ruoliu.dev@gmail.com>
2024-08-17 01:41:23 -0500

    good dp

---
+++ python/dp/sightseeing.py
@@ -0,0 +1,9 @@
+# 1014
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        res = 0
+        cur = 0
+        for i in range(1, len(values)):
+            cur = max(cur, values[i - 1] + i - 1)
+            res = max(res, cur + values[i] - i)
+        return res
