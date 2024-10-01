dde18c51ea6288991bfe5c71786e5b7f363d39e6
Ruo Liu <ruoliu.dev@gmail.com>
2024-09-30 21:02:59 -0500

    ss

--- python/dp/sightseeing.py
+++ python/dp/sightseeing.py
@@ -1,8 +1,7 @@
 # 1014
 class Solution:
-    def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        res = 0
-        cur = 0
+    def maxScoreSightseeingPair(self, values: list[int]) -> int:
+        res = cur = 0
         for i in range(1, len(values)):
             cur = max(cur, values[i - 1] + i - 1)
             res = max(res, cur + values[i] - i)
