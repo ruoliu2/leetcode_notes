15ad9dfe9e02a8da6bf22d73931be8b4a220d70b
Ruo Liu <ruoliu.dev@gmail.com>
2024-11-17 19:31:20 -0600

    change

--- company/amazon/OA/min_to_remove.py
+++
@@ -1,9 +0,0 @@
-def min_to_remove(arr, capacity):
-    n = len(arr)
-    idxs = [-1] * n
-    for i, val in enumerate(arr):
-        idxs[i] = bisect_right(arr, val * 3)
-    res = n
-    for i in range(n):
-        res = min(res, i + n - idxs[i])
-    return res
